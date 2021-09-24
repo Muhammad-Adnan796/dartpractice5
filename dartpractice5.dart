4215825431eb33d11be88521d64ce27424549a69
Muhammad Adnan <79781004+Adnan-Swati@users.noreply.github.com>
2021-09-24 05:09:54 -0700

    Add files via upload

---
+++ dartpractice5.dart
@@ -0,0 +1,16 @@
+void main() {
+//  var a1 = [1,2,'three',4,5,6,7];
+//  a1.clear();
+//   var a2 =a1.last;
+//   var a3 =a1.length;
+//   var a4 =a1.elementAt(3);
+//   var a5 = a1.first;
+//   print ('there are $a2 elements in list'); 
+//   print ('there are $a3 elements in list'); 
+//   print ('there are $a4 elements in list'); 
+//   print ('there are $a5 elements in list'); 
+//   a1[2] = 5;
+//   print (a1);
+//   a1.replaceRange(1,4,{7,8,9});
+//   print (a1);
+  }
